b4bb4ba7a02a79c82f6edc7732d4f398d8a2d058
RALALANANTENAINA Mirana Vivien <miranavvs@gmail.com>
2019-05-26 22:56:28 +0300

    -

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
      hostname:'localhost',
      username:'root',
-     password:'mirana',
+     password:'',
      database:'base_11',
      user_email:'miranavvs@gmail.com',
      user_password :'toto'
